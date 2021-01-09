372b19c9769cf0eb914d6a5b4f6924f5302e462b
Zitiix <77161057+Zitiix@users.noreply.github.com>
2021-01-09 15:21:27 +0000

    Create play.js

---
+++ musique/play.js
@@ -0,0 +1,137 @@
+const { play } = require("../include/play");
+const ytdl = require("ytdl-core");
+const YouTubeAPI = require("simple-youtube-api");
+const scdl = require("soundcloud-downloader").default;
+const https = require("https");
+const { YOUTUBE_API_KEY, SOUNDCLOUD_CLIENT_ID, DEFAULT_VOLUME } = require("../util/MusicfrUtil");
+const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
+const emoji = require("../emojis.json");
+
+module.exports = {
+  name: "play",
+  cooldown: 3,
+  aliases: ["p"],
+  description: "Joue de l'audio depuis YouTube ou Soundcloud",
+  async execute(message, args) {
+    const { channel } = message.member.voice;
+
+    const serverQueue = message.client.queue.get(message.guild.id);
+    if (!channel) return message.reply(`${emoji.no} - Tu dois d'abord être dans un salon vocal!`).catch(console.error);
+    if (serverQueue && channel !== message.guild.me.voice.channel)
+      return message.reply(`${emoji.warning} - Tu dois être dans le même salon vocal que ${message.client.user}!`).catch(console.error);
+
+    if (!args.length)
+      return message
+        .reply(`Usage: ${message.client.prefix}play <YouTube URL | Video Name | Soundcloud URL>`)
+        .catch(console.error);
+
+    const permissions = channel.permissionsFor(message.client.user);
+    if (!permissions.has("CONNECT"))
+      return message.reply(`${emoji.warning} - Je ne peux pas me connecter a ce vocal, il me manque des permissions!`);
+    if (!permissions.has("SPEAK"))
+      return message.reply(`${emoji.warning} - Je ne peux pas parler dans ce salon vocal, faites en sortes que j'ai les permissions!`);
+
+    const search = args.join(" ");
+    const videoPattern = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.?be)\/.+$/gi;
+    const playlistPattern = /^.*(list=)([^#\&\?]*).*/gi;
+    const scRegex = /^https?:\/\/(soundcloud\.com)\/(.*)$/;
+    const mobileScRegex = /^https?:\/\/(soundcloud\.app\.goo\.gl)\/(.*)$/;
+    const url = args[0];
+    const urlValid = videoPattern.test(args[0]);
+
+    if (!videoPattern.test(args[0]) && playlistPattern.test(args[0])) {
+      return message.client.commands.get("playlist").execute(message, args);
+    } else if (scdl.isValidUrl(url) && url.includes("/sets/")) {
+      return message.client.commands.get("playlist").execute(message, args);
+    }
+
+    if (mobileScRegex.test(url)) {
+      try {
+        https.get(url, function (res) {
+          if (res.statusCode == "302") {
+            return message.client.commands.get("play").execute(message, [res.headers.location]);
+          } else {
+            return message.reply(`${emoji.no} - Aucun contenu n'a été trouvé pour cette URL`).catch(console.error);
+          }
+        });
+      } catch (error) {
+        console.error(error);
+        return message.reply(error.message).catch(console.error);
+      }
+      return message.reply("./.").catch(console.error);
+    }
+
+    const queueConstruct = {
+      textChannel: message.channel,
+      channel,
+      connection: null,
+      songs: [],
+      loop: false,
+      volume: DEFAULT_VOLUME || 100,
+      playing: true
+    };
+
+    let songInfo = null;
+    let song = null;
+
+    if (urlValid) {
+      try {
+        songInfo = await ytdl.getInfo(url);
+        song = {
+          title: songInfo.videoDetails.title,
+          url: songInfo.videoDetails.video_url,
+          duration: songInfo.videoDetails.lengthSeconds
+        };
+      } catch (error) {
+        console.error(error);
+        return message.reply(error.message).catch(console.error);
+      }
+    } else if (scRegex.test(url)) {
+      try {
+        const trackInfo = await scdl.getInfo(url, SOUNDCLOUD_CLIENT_ID);
+        song = {
+          title: trackInfo.title,
+          url: trackInfo.permalink_url,
+          duration: Math.ceil(trackInfo.duration / 1000)
+        };
+      } catch (error) {
+        console.error(error);
+        return message.reply(error.message).catch(console.error);
+      }
+    } else {
+      try {
+        const results = await youtube.searchVideos(search, 1);
+        songInfo = await ytdl.getInfo(results[0].url);
+        song = {
+          title: songInfo.videoDetails.title,
+          url: songInfo.videoDetails.video_url,
+          duration: songInfo.videoDetails.lengthSeconds
+        };
+      } catch (error) {
+        console.error(error);
+        return message.reply(error.message).catch(console.error);
+      }
+    }
+
+    if (serverQueue) {
+      serverQueue.songs.push(song);
+      return serverQueue.textChannel
+        .send(`${emoji.yes} - **${song.title}** a été ajouté à la liste d'attente des musiques par ${message.author}`)
+        .catch(console.error);
+    }
+
+    queueConstruct.songs.push(song);
+    message.client.queue.set(message.guild.id, queueConstruct);
+
+    try {
+      queueConstruct.connection = await channel.join();
+      await queueConstruct.connection.voice.setSelfDeaf(true);
+      play(queueConstruct.songs[0], message);
+    } catch (error) {
+      console.error(error);
+      message.client.queue.delete(message.guild.id);
+      await channel.leave();
+      return message.channel.send(`${emoji.warning} - Ne peux pas rejoindre le vocal: ${error}`).catch(console.error);
+    }
+  }
+};
